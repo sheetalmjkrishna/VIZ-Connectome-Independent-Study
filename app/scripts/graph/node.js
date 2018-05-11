16b919fa087972df58ee50776707d18664ce5adb
sheetalmjkrishna <sheetalmjkrishna@gmail.com>
2018-05-11 12:29:39 -0600

    Update 2018-05-11T18:29:37.626Z

--- app/scripts/graph/node.js
+++
@@ -1,8 +0,0 @@
-class Node {
-  constructor(nodeData) {
-    let self = this;
-    self.id = nodeData.StructureID;
-    self.label = nodeData.Label ? nodeData.Label.trim() : 'Unknown';
-    self.tags = nodeData.Tags;
-  }
-}
